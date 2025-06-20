6be9395dcb208fdfa3122480d1beed7232230b97
wisohyun <wisohyun0@gmail.com>
2025-06-20 15:25:43 +0900

    Add get_Abs function

--- calculator.js
+++ calculator.js
@@ -4,4 +4,10 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide_new = (a, b) => a / b;
 const get_Median = (a, b) => (a * b) / 2;
-const get_Remainder = (a, b) => a / b;
+const get_Remainder = (a, b) => a % b;
+const get_Abs = (num) => {
+  if (num >= 0) {
+    return num;
+  }
+  return -num;
+};
