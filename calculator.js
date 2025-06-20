fabfdf473db9034a446875cb6ff51c71bde1cf58
wisohyun <wisohyun0@gmail.com>
2025-06-20 15:53:53 +0900

    Add factorial function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,11 @@ const get_Abs = (num) => {
 };
 const get_Percent = (a, b) => (a / b) * 100;
 const get_Sum_ver1 = (n) => n(n + 1) / 2;
+const factorial = (n) => {
+  let num = 1;
+  while (n >= 1) {
+    num = num * n;
+    n = n - 1;
+  }
+  return num;
+};
